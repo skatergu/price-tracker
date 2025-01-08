78c91f2c45f390e42493134070e35bd5c7970a24
skatergu <skatergu@yahoo.com>
2025-01-08 16:16:07 -0500

    fixed imports, cors heades

--- Backend/app.py
+++ Backend/app.py
@@ -1,11 +1,12 @@
 import subprocess
-from flask import Flask, jsonify, request
+# from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from flask_cors import CORS
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///database.db'
 
 db = SQLAlchemy(app)
